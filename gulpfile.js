e0540a7e069a8e18bc39d186473fadbc9fec7b9e
SergeiChervyakovsky <gghf@mail.ru>
2020-03-03 17:45:50 +0300

    create min.css file

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,14 @@
+// Подключение модулей
+// Сам Gulp
 const gulp = require('gulp');
+// Локальный сервер BrowserSync
 const browserSync = require('browser-sync').create();
+// Минимизатор CSS
+const cleanCSS = require('gulp-clean-css');
+// Переименовываем файлы стилей CSS
+const rename = require("gulp-rename");
 
-gulp.task('hello', function(done) {
-  console.log('Привет мир!');
-  done();
-});
-
-// Static server
+// Сервер BrowserSync
 gulp.task('browser-sync', function() {
   browserSync.init({
       server: {
@@ -17,3 +19,21 @@ gulp.task('browser-sync', function() {
 });
 
 
+//Задаём таск на стили CSS второе название может быть любым названием команды
+gulp.task('styles', styles);
+
+function styles() {
+  // Рабочий файл CSS
+  return gulp.src('css/*.css')
+  // Минимизация
+  .pipe(cleanCSS({
+    level: 2
+}))
+// Переименование с суффиксом мин
+.pipe(rename({
+  suffix: ".min",
+  extname: ".css"
+}))
+// Путь куда его кладём
+  .pipe(gulp.dest('mincss'))
+}
