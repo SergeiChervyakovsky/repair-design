90e205eb362cf688e6049134313e4c85744c2870
SergeiChervyakovsky <gghf@mail.ru>
2020-03-04 15:14:46 +0300

    feat:add gulp-sass in gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,32 @@
 // Подключение модулей
 // Сам Gulp
-const gulp = require('gulp');
+const {src, dest, watch} = require('gulp');
 // Локальный сервер BrowserSync
 const browserSync = require('browser-sync').create();
 // Минимизатор CSS
 const cleanCSS = require('gulp-clean-css');
 // Переименовываем файлы стилей CSS
 const rename = require("gulp-rename");
+// подключение Sass
+const sass = require("gulp-sass");
 
 // Сервер BrowserSync
-gulp.task('browser-sync', function() {
+function bs() {
+  serveSass();
   browserSync.init({
       server: {
           baseDir: "./"
       }
   });
-  gulp.watch("./*.html").on('change', browserSync.reload);
-});
+  watch("./*.html").on('change', browserSync.reload);
+  watch("./sass/**/*.sass").on('change', serveSass);
+  watch("./js/*.js").on('change', browserSync.reload);
+};
 
-
-//Задаём таск на стили CSS второе название может быть любым названием команды
-gulp.task('styles', styles);
 
 function styles() {
   // Рабочий файл CSS
-  return gulp.src('css/*.css')
+  return src('css/*.css')
   // Минимизация
   .pipe(cleanCSS({
     level: 2
@@ -35,5 +37,13 @@ function styles() {
   extname: ".css"
 }))
 // Путь куда его кладём
-  .pipe(gulp.dest('dist'))
+  .pipe(dest('dist'))
 }
+
+ function serveSass() {
+  return src("/sass/*.sass")
+      .pipe(sass())
+      .pipe(dest("./css"))
+      .pipe(browserSync.stream());
+};
+ exports.serve = bs;
